62e6e96862451cbbaa7e9b280b3dbf3dd372ae3d
suthanRcs <suthanmsd07@gmail.com>
2025-04-19 16:09:49 +0530

    apr 19 works

--- src/Components/NabarComp.tsx
+++ src/Components/NabarComp.tsx
@@ -1,19 +1,16 @@
 'use client';
 
-import Logo from "/BCL-Green-1.svg"
-import Frame from "../../public/Frame.svg"
-import Search from "../../public/Framesearch (1).svg"
-import { Menu, Dropdown, Button, Badge, Avatar } from 'antd';
+
+import { Menu, Dropdown, Button, Badge, Avatar, Input } from 'antd';
 import { DownOutlined, UserOutlined } from '@ant-design/icons';
 import "../styles/userprofile.scss";
+import { SearchOutlined } from '@ant-design/icons';
+
 
 const NavbarComp = () => {
     const menu = (
         <Menu>
-            {/* <Menu.Item key="1">Profile</Menu.Item>
-            <Menu.Item key="2">Settings</Menu.Item>
-            <Menu.Divider />
-            <Menu.Item key="3">Logout</Menu.Item> */}
+
 
             <div className="drop-down ">
 
@@ -22,7 +19,7 @@ const NavbarComp = () => {
 
                     <div className="logo-username ">
 
-                        <img src="/user.png" alt="username"  className="h-[40px] w-[40px]"/>
+                        <img src="/user.png" alt="username" className="h-[40px] w-[40px]" />
                         <div className="span-useername-div">
                             <span >Mohamed Uvaish</span><br></br>
                             <span>Uvaishmoahamedn@gmail.com</span>
@@ -38,34 +35,34 @@ const NavbarComp = () => {
 
 
 
-                        <div className="small-logo">
+                    <div className="small-logo">
 
-                        <img src="/user.png" alt="username"  className="h-[20px] w-[20px]"/>
-                            <span>Profile</span>
-                        </div>
+                        <img src="/user.png" alt="username" className="h-[20px] w-[20px]" />
+                        <span>Profile</span>
+                    </div>
 
-                        <div className="small-logo">
+                    <div className="small-logo">
 
-                        <img src="/checkout.png" alt="username"  className="h-[20px] w-[20px]"/>
-                            <span>My Orders</span>
-                        </div>
-
-
-                        <div className="small-logo">
-
-                        <img src="/bill.png" alt="username"  className="h-[20px] w-[20px]"/>
-                            <span>Billing</span>
-                        </div>
+                        <img src="/checkout.png" alt="username" className="h-[20px] w-[20px]" />
+                        <span>My Orders</span>
+                    </div>
 
 
+                    <div className="small-logo">
+
+                        <img src="/bill.png" alt="username" className="h-[20px] w-[20px]" />
+                        <span>Billing</span>
+                    </div>
 
 
 
-                        <div className="small-logo">
 
-                        <img src="/location.png" alt="username"  className="h-[20px] w-[20px]"/>
-                            <span>Address</span>
-                        </div>
+
+                    <div className="small-logo">
+
+                        <img src="/location.png" alt="username" className="h-[20px] w-[20px]" />
+                        <span>Address</span>
+                    </div>
 
 
                     <div className="line"></div>
@@ -73,14 +70,15 @@ const NavbarComp = () => {
 
                     <div className="small-logoout">
 
-                    <img src="/logout.png" alt="username"  className="h-[20px] w-[20px]"/>
-                    <span>Log Out</span>
+                        <img src="/logout.png" alt="username" className="h-[20px] w-[20px]" />
+                        <span>Log Out</span>
                     </div>
                 </div>
 
             </div>
         </Menu>
     );
+
     return (
         <div className="navbar-container  ">
 
@@ -92,24 +90,42 @@ const NavbarComp = () => {
                 <div className="delivery-text">
 
                     <span>Delivery in 8 minutes</span><br></br>
-                    <span>Select Location </span>
+                    <div className="drop-nav">
+                        <span>Select Location </span>
+                        <img src="./Border.svg" alt="drop" />
+
+                    </div>
+
+
                 </div>
 
                 <div className="navbar-input">
 
-                    <input className="search-input" type="text" placeholder="Search by Tomato....." />
+
+
+
+                    <Input
+
+                        className="tomato-search-input"
+                        placeholder="Search by Tomato....."
+                        prefix={<SearchOutlined />}
+                        style={{ width: 600, height: 45, gap: 7, fontSize: 16, fontWeight: 400 }}
+                    />
 
 
                 </div>
 
                 <div className="login-cart-button">
-                    
-            <Dropdown overlay={menu} trigger={['click']} placement="bottomRight">
-                <Badge dot color="green" className="avatar-badge">
-                    <Avatar shape="circle" icon={<UserOutlined />} />
-                </Badge>
-            </Dropdown>
-                    <button className="cart-button" >  <img src={Frame} alt="" /> Cart</button>
+
+                    <Dropdown overlay={menu} trigger={['click']} placement="bottomRight">
+                        <Badge dot color="green" className="avatar-badge">
+                            <Avatar shape="circle" icon={<UserOutlined />} />
+                        </Badge>
+                    </Dropdown>
+                    <button className="cart-button" >
+                        <img src="./Vectora.svg" alt="" />
+                        <span>Cart</span>
+                    </button>
 
 
                 </div>
