0cdd24fd5013fb848309139e4649b328234c9172
suthanRcs <suthanmsd07@gmail.com>
2025-04-21 21:42:37 +0530

    apr 21 final works

--- src/Components/SigninCompo.tsx
+++ src/Components/SigninCompo.tsx
@@ -1,6 +1,5 @@
 'use client';
-import Image from "next/image";
-import Logo from "../../public/BCL-Green-1.svg"
+
 import "../styles/Signin.scss";
 import { useRouter } from "next/navigation";
 import { Button, Form, Input } from "antd";
@@ -8,7 +7,6 @@ import { EyeInvisibleOutlined, EyeTwoTone } from "@ant-design/icons";
 import { useState } from "react";
 
 export default function LoginCompo() {
-
     const router = useRouter();
     const [form] = Form.useForm();
     const [login, setLogin] = useState(true);
@@ -19,187 +17,91 @@ export default function LoginCompo() {
 
     return (
         <div className="login-wrapper">
-
             <div className="login-card">
+                <div className="login-form-container">
+                    <img className="brand-login" src="/BCL-Green-1.svg" alt="Logo" />
 
-                {login ?
-                    <div className="login-form-container">
+                    <div className="welcome-message">
+                        <span className="title">
+                            {login
+                                ? "Create Your Account & Start Shopping!"
+                                : "Almost There! Let’s Set Up Your Delivery Details"}
+                        </span>
 
-                        <img className="brand-login" src="/BCL-Green-1.svg" alt="Logo" />
-
-                        <div className="welcome-message">
-                            <span>Create Your Account & Start Shopping!</span>
-
-
-                            <div className="small-text">
-                                <span>Sign up to explore fresh vegetables, add items to your cart, and complete your purchase seamlessly.</span>
-                            </div>
-
-                        </div>
-
-
-
-                        <Form form={form}>
-                            <p className="input-p-texts">Name </p>
-                            <Input type="text" placeholder="Name" className="ant-input" />
-                            <p className="input-p-texts">Email </p>
-                            <Input placeholder="Email" className="ant-input" />
-                            <p className="input-p-texts">Phone Number </p>
-                            <Input type="text" placeholder="phonenumber" className="ant-input" />
-
-                            <p className="input-p-texts">Password sign in</p>
-                            <Input.Password
-                                className="ant-input"
-                                placeholder="Password"
-                                iconRender={(visible) =>
-                                    visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
-                                }
-                            />
-
-                            <p className="input-p-texts">Confirm Password</p>
-                            <Input.Password
-                                className="ant-input"
-                                placeholder="Confirm Password"
-                                iconRender={(visible) =>
-                                    visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
-                                }
-                            />
-
-
-                            <div className="options-row">
-                                {login && (
-                                    <a href="#" className="forgot-password">
-                                        Forgot Password?
-                                    </a>
-                                )}
-                            </div>
-
-
-                            <Button className="sign-in-button text-white" onClick={() => setLogin(!login)} >
-
-                                Continue
-                            </Button>
-
-
-                        </Form>
-
-                        <div className="divider">
-                            <hr className="line" />
-                            <span className="or-text">or</span>
-                            <hr className="line" />
-                        </div>
-
-                        <div className="toggle-login">
-                            <span className="toggle-text">
-                                Already have an account ?
+                        <div className="small-text">
+                            <span>
+                                {login
+                                    ? "Sign up to explore fresh vegetables, add items to your cart, and complete your purchase seamlessly."
+                                    : "To ensure smooth delivery, please provide your address"}
                             </span>
-
-
-                            <a href="#" className="toggle-link"
-                            > Login
-                            </a>
                         </div>
-
                     </div>
 
-                    :
+                    <Form form={form} className="login-form">
+                        {login ? (
+                            <>
+                                <p className="input-label">Name *</p>
+                                <Input type="text" placeholder="Name" className="custom-input" />
+                                <p className="input-label">Email *</p>
+                                <Input placeholder="Email" className="custom-input" />
+                                <p className="input-label">Phone Number *</p>
+                                <Input type="text" placeholder="Phone Number" className="custom-input" />
+                                <p className="input-label">Password *</p>
+                                <Input.Password
+                                    className="custom-input"
+                                    placeholder="Password"
+                                    iconRender={(visible) =>
+                                        visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+                                    }
+                                />
+                                <p className="input-label">Confirm Password *</p>
+                                <Input.Password
+                                    className="custom-input"
+                                    placeholder="Confirm Password"
+                                    iconRender={(visible) =>
+                                        visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+                                    }
+                                />
+                            </>
+                        ) : (
+                            <>
+                                <p className="input-label">Select Country *</p>
+                                <Input placeholder="France" className="custom-input" />
+                                <p className="input-label">Select Address *</p>
+                                <Input type="text" placeholder="Enter your address..." className="custom-input-address" />
+                                <p className="input-label">Zip Code *</p>
+                                <Input type="text" placeholder="Enter your zip code" className="custom-input" />
 
+                                <div className="gps-container ">
+                                    <img src="/Vector.svg" alt="logo" />
 
-                    <div className="login-form-container">
-
-                        <img className="brand-login" src="/BCL-Green-1.svg" alt="Logo" />
-
-                        <div className="welcome-message">
-                            <span>Almost There! Let’s Set Up Your Delivery Details</span>
-
-                            <div className="small-text">
-                                <span>  To ensure smooth delivery, please provide your address</span>
-
-                            </div>
-
-                        </div>
-
-
-
-                        <Form form={form}>
-
-                            <p className="input-p-texts">Select Country* </p>
-                            <Input type="" placeholder="France" className="ant-input" />
-                            <p className="input-p-texts">Select Address*</p>
-                            <Input type="text" placeholder="Enter your address..." className="ant-input-address" />
-
-                            <p className="input-p-texts">Zip code*</p>
-                            <Input type="text" placeholder="Enter your zip code" className="ant-input" />
-
-
-                            <div className="gps flex flex-row mt-8">
-
-                                <div className="gps-logo"><img src="/Vector.svg" alt="logo" /></div>
-                                <div className="flex flex-col ml-3">
-                                    <span className="current-gps">Current Location</span>
-                                    <span className="using-gps">Using GPS</span>
+                                    <div className="gps-text ">
+                                        <span className="current-gps">Current Location</span> <br></br>
+                                        <span className="using-gps">Using GPS</span>
+                                    </div>
                                 </div>
-                            </div>
+                            </>
+                        )}
 
 
 
+                        <Button className="sign-in-button" onClick={() => setLogin(!login)}>
+                            {login ? "Continue" : "Register"}
+                            {!login && <img className="arrow-logo" src="/arrow.svg" alt="arrow" />}
+                        </Button>
+                    </Form>
 
-                            <div className="options-row">
-                                {login && (
-                                    <a href="#" className="forgot-password">
-                                        Forgot Password?
-                                    </a>
-                                )}
-                            </div>
-
-
-                            <Button className="sign-in-button !text-white" onClick={() => setLogin(!login)} >
-                                {login ? "Continue" : "Register"}
-                                <img className="arrow-logo" src="/arrow.svg" alt="brand" />
-                            </Button>
-
-
-                        </Form>
-
-                        <div className="divider">
-                            <hr className="line" />
-                            <span className="or-text">or</span>
-                            <hr className="line" />
-                        </div>
-
-                        <div className="toggle-login">
-                            <span className="toggle-text">
-                                Already have an account ?
-                            </span>
-
-                            {/* <span className="toggle-link"> Register</span> */}
-
-                            <a href="#" className="toggle-link"
-                            > Login
-                            </a>
-                        </div>
-
+                    <div className="divider">
+                        <hr className="line" />
+                        <span className="or-text">or</span>
+                        <hr className="line" />
                     </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                }
+                    <div className="toggle-login">
+                        <span className="toggle-text">Already have an account ?</span>
+                        <a href="#" className="toggle-link">Login</a>
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/Components/UserprofieCompo.tsx
+++ src/Components/UserprofieCompo.tsx
@@ -1,126 +1,35 @@
 'use client';
 
-import { Input, Select, Badge, Avatar } from "antd";
-
+import { Input } from "antd";
 import "../styles/userprofile.scss";
 
-import { Menu, Dropdown, Button } from 'antd';
-import { DownOutlined, UserOutlined } from '@ant-design/icons';
-
 const Usercompo = () => {
-
-    const menu = (
-        <Menu>
-
-
-            <div className="drop-down ">
-
-                <div className="drop-down-inside ">
-
-
-                    <div className="logo-username ">
-
-                        <img src="" alt="username" />
-                        <div className="span-useername-div">
-                            <span >Mohamed Uvaish</span><br></br>
-                            <span>Uvaishmoahamedn@gmail.com</span>
-
-                        </div>
-
-
-                    </div>
-
-                    <div className="line"></div>
-
-
-                    <div className="button-user">
-
-                        <div className="small-logo">
-
-                            <img src="" alt="logo" />
-                            <span>Profile</span>
-                        </div>
-
-                        <div className="small-logo">
-
-                            <img src="" alt="logo" />
-                            <span>My Orders</span>
-                        </div>
-
-
-                        <div className="small-logo">
-
-                            <img src="" alt="logo" />
-                            <span>Billing</span>
-                        </div>
-
-
-
-
-
-                        <div className="small-logo">
-
-                            <img src="" alt="logo" />
-                            <span>Address</span>
-                        </div>
-
-
-                    </div>
-
-                    <div className="line"></div>
-
-
-                    <div className="small-logoout">
-
-                        <img src="" alt="logo" />
-                        <span>Log Out</span>
-                    </div>
-
-                </div>
-
-            </div>
-        </Menu>
-    );
     return (
-
-
-
-
-        <div className=" h-[100vh] w-full flex flex-col items-center justify-evenly">
-
-            <div className="p-3 w-[75%] text-[black] flex flex-col items-start justify-center gap-4">
-
-
-                <span className="font-[700] text-[20px]">My Account</span>
-                <p>Manage your account settings and preferences.</p>
-
+        <div className="user-wrapper">
+            <div className="account-header">
+                <span className="account-title">My Account</span>
+                <p className="account-description">Manage your account settings and preferences.</p>
             </div>
 
+            <div className="profile-card">
+                <span className="profile-title">Profile</span>
 
-            <div className="p-8 w-[75%] border-1 border-[#CCCCCC] rounded-md">
-                <span className="text-[black] font-[700] ml-6 text-[18px]">Profile</span>
+                <div className="profile-inputs">
+                    <p className="input-label">Name*</p>
+                    <Input placeholder="Name" className="ant-input" />
 
-                <div className="profile-inputs p-6">
-                    <p className="input-p-texts">Name* </p>
-                    <Input type="" placeholder="Name" className="ant-input" />
+                    <p className="input-label">Email</p>
+                    <Input placeholder="Email" className="ant-input" />
 
-                    <p className="input-p-texts">Email</p>
-                    <Input type="" placeholder="Email" className="ant-input" />
-                    <p className="input-p-texts">Phone Number </p>
-                    <Input type="" placeholder="France" className="ant-input" />
-                    <p className="input-p-texts">Address </p>
-                    <Input type="" placeholder="France" className="ant-input" />
+                    <p className="input-label">Phone Number</p>
+                    <Input placeholder="France" className="ant-input" />
 
-
-
-
-
+                    <p className="input-label">Address</p>
+                    <Input placeholder="France" className="ant-input" />
                 </div>
             </div>
         </div>
-
-    )
+    );
 }
 
 export default Usercompo;
-
