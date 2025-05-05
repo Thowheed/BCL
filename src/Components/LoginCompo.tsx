be5b8421b60de3f02b1113bf0a35e6210b0ace58
suthanRcs <suthanmsd07@gmail.com>
2025-05-05 18:59:23 +0530

    may 5 works

--- src/Components/LoginCompo.tsx
+++ src/Components/LoginCompo.tsx
@@ -9,7 +9,23 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { loginListLoad } from "@/store/reducer/indexSlice";
 
+
+const tempLogin = {
+
+    email: "",
+    password: "",
+
+}
+
+
+
 export default function LoginCompo() {
+
+
+    const [TempLoginData, setnewloginData] = useState(tempLogin)
+
+    console.log(TempLoginData);
+
 
     const router = useRouter();
     const [form] = Form.useForm();
@@ -30,11 +46,10 @@ export default function LoginCompo() {
     const Loginapi = () => {
 
 
-
         let payload = {
 
-            email: "1",
-            password: "1",
+            email: TempLoginData?.email,
+            password: TempLoginData?.password,
         }
 
 
@@ -42,77 +57,79 @@ export default function LoginCompo() {
 
     }
 
-    useEffect(()=>{
 
-        Loginapi()
+    return (
+        <div className="login-wrapper">
 
-    },[])
+            <div className="login-card">
 
+                <div className="login-form-container">
 
+                    <img className="brand-login" src="/BCL-Green-1.svg" alt="Logo" />
 
-
-return (
-    <div className="login-wrapper">
-
-        <div className="login-card">
-
-            <div className="login-form-container">
-
-                <img className="brand-login" src="/BCL-Green-1.svg" alt="Logo" />
-
-                <div className="welcome-message">
-                    <span>Complete Your Purchase – Log In to Continue!" </span>
-                    <div className="small-text">
-                        <span>To proceed with payment and enjoy a seamless shopping experience, please log in to your account.</span>
+                    <div className="welcome-message">
+                        <span>Complete Your Purchase – Log In to Continue!" </span>
+                        <div className="small-text">
+                            <span>To proceed with payment and enjoy a seamless shopping experience, please log in to your account.</span>
+                        </div>
                     </div>
-                </div>
 
 
 
-                <Form form={form}>
-                    <p className="input-labels">Email </p>
-                    <Input placeholder="Email" className="ant-input" />
-                    <p className="input-labels">Password  </p>
-                    <Input.Password
-                        className="ant-input"
-                        placeholder="Password"
-                        iconRender={(visible) =>
-                            visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
-                        }
-                    />
+                    <Form form={form} onFinish={Loginapi}>
 
-                    <div className="options-row">
-                        {login && (
-                            <a href="#" className="forgot-password">
-                                Forgot Password?
-                            </a>
-                        )}
+                        <p className="input-labels">Email </p>
+                        <Input placeholder="Email" className="ant-input" onChange={(e: any) => setnewloginData({
+                            ...TempLoginData,
+                            email: e.target.value
+                        })} />
+                        <p className="input-labels">Password  </p>
+                        <Input.Password
+                            className="ant-input"
+                            placeholder="Password"
+                            iconRender={(visible) =>
+                                visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+                            }
+                            onChange={(e: any) => setnewloginData({
+                                ...TempLoginData,
+                                password: e.target.value
+                            })}
+                        />
+
+                        <div className="options-row">
+                            {login && (
+                                <a href="#" className="forgot-password">
+                                    Forgot Password?
+                                </a>
+                            )}
+                        </div>
+                        <Button className="sign-in-button" htmlType="submit" type="primary">
+                            Login
+                        </Button>
+
+
+                    </Form>
+
+                    <div className="divider">
+                        <hr className="line" />
+                        <span className="or-text">or</span>
+                        <hr className="line" />
                     </div>
-                    <Button className="sign-in-button" >
-                        Login
-                    </Button>
-                </Form>
 
-                <div className="divider">
-                    <hr className="line" />
-                    <span className="or-text">or</span>
-                    <hr className="line" />
+                    <div className="toggle-login">
+                        <span className="toggle-text">
+                            Don't have an account ?
+                        </span>
+
+                        {/* <span className="toggle-link"> Register</span> */}
+
+                        <a href="#" className="toggle-link"
+                            onClick={() => setLogin(!login)} > Register
+                        </a>
+                    </div>
+
                 </div>
-
-                <div className="toggle-login">
-                    <span className="toggle-text">
-                        Don't have an account ?
-                    </span>
-
-                    {/* <span className="toggle-link"> Register</span> */}
-
-                    <a href="#" className="toggle-link"
-                        onClick={() => setLogin(!login)} > Register
-                    </a>
-                </div>
-
             </div>
         </div>
-    </div>
-);
+    );
 }
--- src/Components/UserprofieCompo.tsx
+++ src/Components/UserprofieCompo.tsx
@@ -4,9 +4,29 @@ import { Input } from "antd";
 import "../styles/userprofile.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { updateuserListLoad } from "@/store/reducer/indexSlice";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+
+
+const Tempfrm = {
+    id:"",
+    name: "",
+    email: "",
+    mobileNo: "",
+    password: "",
+    address: "",
+    temp_address: "",
+    country: "",
+    zip_code: "",
+    location: ""
+
+}
 
 const Usercompo = () => {
+
+    const [TempData, setFormData] = useState(Tempfrm);
+
+
 
     const { updateuserLoad, updateuserData } = useSelector((state: any) => state.bcl);
     console.log(updateuserLoad);
@@ -17,15 +37,15 @@ const Usercompo = () => {
     const updateUserapi = () => {
 
         let payload = {
-            
-            id:1,
-            name: "1",
-            email: "1",
-            password: "1",
-            address: "1",
-            country: "1",
-            zip_code: "1",
-            location: "1"
+
+            id: Tempfrm?.id,
+            name: Tempfrm?.name,
+            email: Tempfrm?.email,
+            password:Tempfrm?.password ,
+            address:Tempfrm?.address,
+            country: Tempfrm?.country,
+            zip_code: Tempfrm?.zip_code,
+            location: Tempfrm?.location
         }
 
         dispatch(updateuserListLoad(payload))
@@ -49,17 +69,27 @@ const Usercompo = () => {
                 <span className="profile-title">Profile</span>
 
                 <div className="profile-inputs">
-                    <p className="input-label">Name*</p>
+                    <p className="input-label">Name *</p>
                     <Input placeholder="Name" className="ant-input" />
 
-                    <p className="input-label">Email</p>
+                    <p className="input-label">Email *</p>
                     <Input placeholder="Email" className="ant-input" />
 
-                    <p className="input-label">Phone Number</p>
-                    <Input placeholder="France" className="ant-input" />
+                    <p className="input-label">Phone Number *</p>
+                    <Input placeholder="Phone Number" className="ant-input" />
 
-                    <p className="input-label">Address</p>
-                    <Input placeholder="France" className="ant-input" />
+                    <p className="input-label">Address *</p>
+                    <Input placeholder="address" className="ant-input" />
+
+                    <p className="input-label">Temp-Address *</p>
+                    <Input placeholder="temp-address" className="ant-input" />
+
+
+                    <p className="input-label">Zip-code *</p>
+                    <Input placeholder="zip-Code" className="ant-input" />
+
+
+
                 </div>
             </div>
         </div>
