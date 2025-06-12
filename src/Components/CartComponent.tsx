b0768f2304b91772f5008b3865ff288393915137
suthanRcs <suthanmsd07@gmail.com>
2025-06-12 17:18:20 +0530

    all design correction completd jun123333

--- src/Components/CartComponent.tsx
+++ src/Components/CartComponent.tsx
@@ -94,11 +94,14 @@ const CartComponent = () => {
                 <div className='flex sm:flex-row justify-center mt-10 flex-col'>
                     <CartTable />
                     <CartTotal />
+<<<<<<< HEAD
 
                     {/* {/* <Elements stripe={stripePromise} options={options}> */}
                         <CartTotal />
                     {/* </Elements> */} */}
 
+=======
+>>>>>>> 74772ba (all design correction completd jun12)
                 </div>
 
                 <div className=' mx-5 my-5 sm:my-2  sm:mx-40 '>
--- src/Components/CartTotal.tsx
+++ src/Components/CartTotal.tsx
@@ -11,7 +11,11 @@ import { loadStripe } from "@stripe/stripe-js";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
 const CartTotal = () => {
+<<<<<<< HEAD
   // const stripe = useStripe();
   // const elements = useElements();
   // const [errorMessage, setErrorMessage] = useState<string | null>(null);
@@ -52,6 +56,56 @@ const CartTotal = () => {
         'Content-Type': 'application/json',
       },
     });
+=======
+  return (
+    <div className="cart-total-container">
+      <div className="total-grams-container flex flex-row items-center justify-between">
+        <div className="text-bold">Total Kgs</div>
+        <div>4.5kg</div>
+      </div>
+
+      <div className="mt-5">
+        <Alert type="warning" message="Need minimum 5.0 kg to proceed to your order" />
+      </div>
+
+      <div className="bill-details-container mt-5 text-bold">Bill Details</div>
+
+      <div className="total-item-container flex flex-row items-center justify-between mt-3">
+        <div className="total-item flex flex-row items-center">
+          <Image src={appImages?.TOTAL_ITEM_IMAGE} alt="Discount" width={20} height={20} />
+          <div>Total Items</div>
+        </div>
+        <div className="total-item-value">$250</div>
+      </div>
+
+      <div className="total-item-container flex flex-row items-center justify-between mt-3">
+        <div className="total-item flex flex-row items-center">
+          <Image src={appImages?.TOTAL_ITEM_IMAGE} alt="Discount" width={20} height={20} />
+          <div>Delivery Charges</div>
+        </div>
+        <div className="total-item-value">$50</div>
+      </div>
+
+      <div className="Address-details-container mt-5">
+        <div className="text-bold">Address Details</div>
+
+        <div className="address-payment-container flex flex-col items-center justify-between mt-3">
+          <div className="address-details flex flex-row items-center justify-between w-[90%]">
+            <div className="flex flex-row items-center">
+              <Image src={appImages?.LOCATION_ICON} alt="Location" width={30} height={30} />
+              <div className="my-3 mx-2">Willsiong street, Church colony,<br />United Kingdom</div>
+            </div>
+            <Image src={appImages?.EDIT_ICON} alt="Edit" width={30} height={30} />
+          </div>
+
+          <button type="submit" className="payment-button flex flex-row items-center justify-between mt-5" >
+            <div>Proceed to payment</div>
+            <ArrowRightOutlined />
+            <div>$300 Total</div>
+          </button>
+
+
+>>>>>>> 74772ba (all design correction completd jun12)
 
   //   if (error) {
   //     setErrorMessage(error.message || "Payment confirmation error");
@@ -98,6 +152,7 @@ const CartTotal = () => {
           <Image src={appImages?.TOTAL_ITEM_IMAGE} alt="Discount" width={20} height={20} />
           <div>Total Items</div>
         </div>
+<<<<<<< HEAD
         <div className="total-item-value">$250</div>
       </div>
 
@@ -159,6 +214,13 @@ const CartTotal = () => {
     </div>
   );
 };
+=======
+      </div>
+    </div>
+  );
+};
+
+>>>>>>> 74772ba (all design correction completd jun12)
 
 
 
